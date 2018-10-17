3c3cd0c56f1066141b43e0d37b383c88225f5a59
DANJOUX MICKAEL p1602272 <mickael.danjoux@etu.univ-lyon1.fr>
2018-10-17 18:53:45 +0200

    test

--- data/character.js
+++ data/character.js
@@ -1,7 +1,19 @@
 var character[
 
   {
-    "nom": "hubert bonisseur de la bath",
+    "nom": "Hubert Bonisseur de la bath",
     "url": "../img/hubert.png"
-   }
+  },
+  {
+    "nom": "Princesse d'Al Taourk",
+    "url": "../img/princesse.jpg"
+  },
+  {
+    "nom": "Larmina",
+    "url": "../img/larmina.png"
+  },
+  {
+    "nom": "Gerhard Moeller",
+    "url": "../img/moeller.png"
+  },
 ]
